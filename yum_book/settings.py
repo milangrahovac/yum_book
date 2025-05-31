a203f3b5e10e71af1f370e18b975223da1cfe910
milan <grahovac85@gmail.com>
2025-05-31 20:28:47 +0200

    added docker yaml

--- yum_book/settings.py
+++ yum_book/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u9wmsljvqnepm0rmxqh)u61)6&9by*$up!w2g3@x()^+i#foc('
+# SECRET_KEY = 'django-insecure-u9wmsljvqnepm0rmxqh)u61)6&9by*$up!w2g3@x()^+i#foc('
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'local-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
