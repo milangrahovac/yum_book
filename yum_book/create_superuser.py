74e1515d1519301d426afc3b8aaefc2758bc8f3e
milan <grahovac85@gmail.com>
2025-06-07 21:14:45 +0200

    fixed  job  yaml

--- yum_book/create_superuser.py
+++ yum_book/create_superuser.py
@@ -1,3 +1,9 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yum_book.settings')  # your settings module
+django.setup()
+
 # create_superuser.py
 from django.contrib.auth import get_user_model
 User = get_user_model()
